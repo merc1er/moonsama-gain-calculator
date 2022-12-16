5f660f800fd39f3baba36ef97fc9f73beb2ad50a
michael <mikeramage.ed@gmail.com>
2022-12-16 15:00:47 -0500

    added khaos price to page

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import Alpine from 'alpinejs'
-import { getUSDPriceOfRss, getUSDPriceOfSama } from './queryprices.js';
+import { getUSDPriceOfRss, getUSDPriceOfSama, getPriceOfARssInSama } from './queryprices.js';
 
 /*material ids correspond to token id
 https://moonsama.com/token/ERC1155/0x1b30a3b5744e733d8d2f19f0812e3f79152a8777/10
@@ -10,9 +10,9 @@ https://moonsama.com/token/ERC1155/0x1b30a3b5744e733d8d2f19f0812e3f79152a8777/10
 
 /* Beta resource materials.
   Beta resources are ERC1155, which are tokens that fall under same address but indexed by ID:
-  erce1155address-tokenId.
+  erce1155address-tokenId. The address field is the erc721 address of the tokens.
 */
-const materials = [{"tokenId": 1, "name":"pumpkins"}, {"tokenId": 2, "name":"blood_crystals"}, {"tokenId": 3, "name":"dna"}, {"tokenId": 4, "name":"mobidium"}, {"tokenId": 5, "name":"wood"}, {"tokenId": 6, "name":"stone"}, {"tokenId": 7, "name":"iron"}, {"tokenId": 8, "name":"gold"}]
+const materials = [{"tokenId": 1, "name":"pumpkins", "address": "0xb296335B6d4AB9F5e5Dc4bf115621c14ab70136C"}, {"tokenId": 2, "name":"blood_crystals", "address": "0x76ba6821A834E2d30A699312829f8204830CCF5B".toLowerCase()}, {"tokenId": 3, "name":"dna", "address": "0xff6ca2a78E4Dd4871256cc5B63dC8dbf02a36F3D".toLowerCase()}, {"tokenId": 4, "name":"mobidium", "address": "0xa1caaaa65fd4e144c4650f87933705b9a065008e"}, {"tokenId": 5, "name":"wood", "address": "0x0528Ae6c997BA4FfC4d6ADD898a2Cc9f26d4a872".toLowerCase()}, {"tokenId": 6, "name":"stone", "address": "0x122EBE2B679cF54Bc8a6e89c1009714b354e2d10".toLowerCase()}, {"tokenId": 7, "name":"iron", "address": "0x829191FEC0f25a27c5bDCdBbD0e37E6342b39945".toLowerCase()}, {"tokenId": 8, "name":"gold", "address": "0x549ab43056E3489335b47927FFcd4825ce484bA3".toLowerCase()}]
 const erc1155address = "0x95370351df734b6a712ba18848b47574d3e90e61";
 
 /**
@@ -21,42 +21,226 @@ const erc1155address = "0x95370351df734b6a712ba18848b47574d3e90e61";
  * @property {string} name - name of token
  * @property {number} lowestSell
  * @property {number} highestBuy
+ * @property {number} khaosExchangePrice
  */
 
 /**
  * Returns prices, tokenIds, and names for all minecraft NFTs
  * @returns {Promise<MaterialResult[]>}
- * @deprecated Use sama network
  */
-async function getAllMaterialsMovr(){
+async function getAllMaterialsSama(){
+  console.log("Get all materials sama...")
   const proms = []
   let rawResult = [];
   for(const material of materials){
-    const prom = getMaterialMovr(material.tokenId)
+    const prom = getMaterialSama(material.tokenId, material.address)
     proms.push(prom)
-    prom.then(({lowestSell, highestBuy})=>{
-      rawResult.push({tokenId: material.tokenId, name: material.name, lowestSell, highestBuy})
+    prom.then(({lowestSell, highestBuy, khaosExchangePrice})=>{
+      rawResult.push({tokenId: material.tokenId, name: material.name, address: material.address, lowestSell, highestBuy, khaosExchangePrice})
     })
   }
   await Promise.all(proms)
   rawResult = rawResult.sort((a, b) => a.tokenId - b.tokenId)
-
-
   const result = {
-    wood: rawResult[0],
-    stone: rawResult[1],
-    iron: rawResult[2],
-    gold: rawResult[3],
-    exp: rawResult[4],
-    grain: rawResult[5],
-    string: rawResult[6],
-    fish_specimen: rawResult[7],
-    moonstone: rawResult[8]
+    pumpkins: rawResult[0],
+    blood_crystals: rawResult[1],
+    dna: rawResult[2],
+    mobidium: rawResult[3],
+    wood: rawResult[4],
+    stone: rawResult[5],
+    iron: rawResult[6],
+    gold: rawResult[7]
   }
   console.log(result)
   return result
 }
 
+/**
+ * Returns the price in SAMA of a token sourced by marketplace.moonsama.com
+ * @param {number} tokenId
+ * @returns {Promise<{lowestSell: number, highestBuy: number}>}
+ */
+async function getMaterialSama(tokenId, address){
+  console.log("get sama material...")
+  let khaosExchangePrice = await Promise.resolve(getPriceOfARssInSama(address));
+
+
+  const graphqlQuery =
+`query getAssetOrders {
+  sells: orders(where: {active: true, sellAsset: "${erc1155address}-${tokenId}", onlyTo: "0x0000000000000000000000000000000000000000"}, orderBy: pricePerUnit, orderDirection: asc, skip: 0, first: 1) {
+    sellAsset {
+      id
+    }
+    askPerUnitNominator
+    askPerUnitDenominator
+  },
+  buys: orders(where: {active: true, buyAsset: "${erc1155address}-${tokenId}"}, orderBy: pricePerUnit, orderDirection: desc, skip: 0, first: 1) {
+    buyAsset {
+      id
+    }
+    askPerUnitNominator
+    askPerUnitDenominator
+  }
+}`
+
+  const fetchUrl = "https://exosama-subgraph.moonsama.com/subgraphs/name/moonsama/marketplacev8" // updated
+  const response = await fetch(fetchUrl, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify({query: graphqlQuery})
+  });
+  const responseJson = await response.json()
+
+  const buys = responseJson.data.buys.map(buy => buy.askPerUnitDenominator/buy.askPerUnitNominator).sort((a, b) => a-b)
+  const sells = responseJson.data.sells.map(sell => sell.askPerUnitNominator/sell.askPerUnitDenominator).sort((a, b) => a-b)
+
+  let lowestSell = sells.shift()
+  let highestBuy = buys.pop()
+
+  if(isNaN(lowestSell)){
+    lowestSell = 0
+  }
+  if(isNaN(highestBuy)){
+    highestBuy = 0
+  }
+  return {lowestSell, highestBuy, khaosExchangePrice}
+}
+
+/**
+ * Returns the total USD value of all resources in SAMA
+ * @param {Dict} resources the player gathered
+ * @param {Dict} prices of resources in SAMA
+ * @param {Float} price of SAMA token in USD: samaPrice
+ * @returns {totalBuySama: number, totalBuyUsd: number, totalSellSama: number, totalSellUsd: number}
+ */
+function getTotalSama(resources, prices, price){
+  let totalBuy = 0
+  let totalSell = 0
+  let totalKhaosDexPrice = 0
+  for(const {tokenId, name} of materials){
+    if(resources.hasOwnProperty(name) && !isNaN(parseFloat(resources[name]))){
+      totalBuy+= resources[name] * prices[name].highestBuy
+      totalSell+= resources[name] * prices[name].lowestSell
+      totalKhaosDexPrice+= resources[name] * prices[name].khaosExchangePrice
+    }
+  }
+
+  const totalBuyUsd = totalBuy * price
+  const totalSellUsd = totalSell * price
+  const totalKhaosDexPriceUSD = totalKhaosDexPrice * price
+
+  return {totalBuy: totalBuy.toFixed(3), totalBuyUsd: totalBuyUsd.toFixed(2), totalSell: totalSell.toFixed(3), totalSellUsd: totalSellUsd.toFixed(2), totalKhaosDexPrice: totalKhaosDexPrice.toFixed(2), totalKhaosDexPriceUSD: totalKhaosDexPriceUSD.toFixed(2)}
+}
+
+/**
+ * Make a date more human-readable
+ * @param {Date} date: the date that needs to be prettified (3 April 2022 format)
+ * @returns {String}
+ */
+function formatDate(date){
+  //keep time zone in UTC so date shows carnage dates as reflected on moonsama website
+  const options = {year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC'}
+  return new Date(date).toLocaleDateString("en-GB", options)
+}
+
+/**
+ * Format date for api request
+ * @param {Date} date: the date that needs to be prettified (yyyy-mm-dd format)
+ * @returns {String}
+ */
+function formatDateApi(date){
+  return new Date(date).toISOString().split('T')[0]
+}
+
+/**
+ * Capitalize and rename resources for human friendly display
+ * @param {String}
+ * @returns {String}
+ */
+function prettifyResource(resource){
+  let res = resource
+
+  if(res === "fish_specimen"){
+    res = "fish"
+  }
+
+  return res.trim().toLowerCase().replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())))
+}
+
+/**
+ * @returns {number[]}
+ */
+function carnageDates() {
+  //2022-04-03 first date that carnage api has, game finishes at 6pm UTC
+  const carnageStartTime = 1649008800000
+  let carnageTime = carnageStartTime
+  const nowTime = new Date().getTime()
+  const dates = [carnageTime];
+  while(true){
+    carnageTime = carnageTime + 1000 * 60 * 60 * 24 * 7
+
+    if(carnageTime > nowTime){
+      break
+    }
+    dates.push(carnageTime)
+  }
+  return dates.reverse()
+}
+
+/**
+ * Add thousands separators
+ * @param {Number}
+ * @returns {String}
+ */
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+/**
+ * Gets valid resources
+ * @param {Object}
+ * @returns {String[]}
+ */
+function getValidResources(resources) {
+  const validResources = []
+    for(const [resource, value] of Object.entries(resources)){
+      if(!isNaN(parseFloat(value))){
+        validResources.push(resource)
+      }
+    }
+    // console.log(validResources)
+    return validResources.sort()
+}
+
+/**
+ * Returns the total USD value of all resources in MOVR
+ * @param {Dict} resources
+ * @param {Dict} prices
+ * @param {Float} price of MOVR token in USD: movrPrice
+ * @returns {totalBuyMovr: number, totalBuyUsd: number, totalSellMovr: number, totalSellUsd: number}
+ * @deprecated The network is now Exosama Network. This network uses $SAMA as the base currency. Use getUSDPriceOfRss in ./queryprices.js.
+ */
+function getTotal(resources, prices, movrPrice){
+  let totalBuyMovr = 0
+  let totalSellMovr = 0
+  for(const {tokenId, name} of materials){
+    if(resources.hasOwnProperty(name) && !isNaN(parseFloat(resources[name]))){
+      totalBuyMovr+= resources[name] * prices[name].highestBuy
+      totalSellMovr+= resources[name] * prices[name].lowestSell
+    }
+  }
+
+  const totalBuyUsd = totalBuyMovr * movrPrice
+  const totalSellUsd = totalSellMovr * movrPrice
+  return {totalBuyMovr: totalBuyMovr.toFixed(3), totalBuyUsd: totalBuyUsd.toFixed(2), totalSellMovr: totalSellMovr.toFixed(3), totalSellUsd: totalSellUsd.toFixed(2)}
+}
 
 /**
  * Returns the price in MOVR of token
@@ -115,15 +299,16 @@ async function getMaterialMovr(tokenId){
 /**
  * Returns prices, tokenIds, and names for all minecraft NFTs
  * @returns {Promise<MaterialResult[]>}
+ * @deprecated Use sama network
  */
-async function getAllMaterialsSama(){
+async function getAllMaterialsMovr(){
   const proms = []
   let rawResult = [];
   for(const material of materials){
-    const prom = getMaterialSama(material.tokenId)
+    const prom = getMaterialMovr(material.tokenId)
     proms.push(prom)
     prom.then(({lowestSell, highestBuy})=>{
-      rawResult.push({tokenId: material.tokenId, name: material.name, lowestSell, highestBuy})
+      rawResult.push({tokenId: matFerial.tokenId, name: material.name, lowestSell, highestBuy})
     })
   }
   await Promise.all(proms)
@@ -131,230 +316,30 @@ async function getAllMaterialsSama(){
 
 
   const result = {
-    pumpkins: rawResult[0],
-    blood_crystals: rawResult[1],
-    dna: rawResult[2],
-    mobidium: rawResult[3],
-    wood: rawResult[4],
-    stone: rawResult[5],
-    iron: rawResult[6],
-    gold: rawResult[7]
+    wood: rawResult[0],
+    stone: rawResult[1],
+    iron: rawResult[2],
+    gold: rawResult[3],
+    exp: rawResult[4],
+    grain: rawResult[5],
+    string: rawResult[6],
+    fish_specimen: rawResult[7],
+    moonstone: rawResult[8]
   }
   console.log(result)
   return result
 }
 
+/* Make the following functions accessible from AlpineJS */
 
-/**
- * Returns the price in SAMA of a token sourced by marketplace.moonsama.com
- * @param {number} tokenId
- * @returns {Promise<{lowestSell: number, highestBuy: number}>}
- */
-async function getMaterialSama(tokenId){
-  const graphqlQuery =
-`query getAssetOrders {
-  sells: orders(where: {active: true, sellAsset: "${erc1155address}-${tokenId}", onlyTo: "0x0000000000000000000000000000000000000000"}, orderBy: pricePerUnit, orderDirection: asc, skip: 0, first: 1) {
-    sellAsset {
-      id
-    }
-    askPerUnitNominator
-    askPerUnitDenominator
-  },
-  buys: orders(where: {active: true, buyAsset: "${erc1155address}-${tokenId}"}, orderBy: pricePerUnit, orderDirection: desc, skip: 0, first: 1) {
-    buyAsset {
-      id
-    }
-    askPerUnitNominator
-    askPerUnitDenominator
-  }
-}`
-
-  const fetchUrl = "https://exosama-subgraph.moonsama.com/subgraphs/name/moonsama/marketplacev8" // updated
-  const response = await fetch(fetchUrl, {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-    body: JSON.stringify({query: graphqlQuery})
-  });
-  const responseJson = await response.json()
-  const buys = responseJson.data.buys.map(buy => buy.askPerUnitDenominator/buy.askPerUnitNominator).sort((a, b) => a-b)
-  const sells = responseJson.data.sells.map(sell => sell.askPerUnitNominator/sell.askPerUnitDenominator).sort((a, b) => a-b)
-
-  let lowestSell = sells.shift()
-  let highestBuy = buys.pop()
-
-  if(isNaN(lowestSell)){
-    lowestSell = 0
-  }
-  if(isNaN(highestBuy)){
-    highestBuy = 0
-  }
-  return {lowestSell, highestBuy}
-}
-
-
-
-/**
- * Returns the total USD value of all resources in MOVR
- * @param {Dict} resources
- * @param {Dict} prices
- * @param {Float} price of MOVR token in USD: movrPrice
- * @returns {totalBuyMovr: number, totalBuyUsd: number, totalSellMovr: number, totalSellUsd: number}
- * @deprecated The network is now Exosama Network. This network uses $SAMA as the base currency. Use getUSDPriceOfRss in ./queryprices.js.
- */
-function getTotal(resources, prices, movrPrice){
-  let totalBuyMovr = 0
-  let totalSellMovr = 0
-  for(const {tokenId, name} of materials){
-    if(resources.hasOwnProperty(name) && !isNaN(parseFloat(resources[name]))){
-      totalBuyMovr+= resources[name] * prices[name].highestBuy
-      totalSellMovr+= resources[name] * prices[name].lowestSell
-    }
-  }
-
-  const totalBuyUsd = totalBuyMovr * movrPrice
-  const totalSellUsd = totalSellMovr * movrPrice
-  return {totalBuyMovr: totalBuyMovr.toFixed(3), totalBuyUsd: totalBuyUsd.toFixed(2), totalSellMovr: totalSellMovr.toFixed(3), totalSellUsd: totalSellUsd.toFixed(2)}
-}
-
-/**
- * Returns the total USD value of all resources in SAMA
- * @param {Dict} resources
- * @param {Dict} prices
- * @param {Float} price of SAMA token in USD: samaPrice
- * @returns {totalBuySama: number, totalBuyUsd: number, totalSellSama: number, totalSellUsd: number}
- */
-function getTotalSama(resources, prices, samaPrice){
-  let totalBuy = 0
-  let totalSell = 0
-  for(const {tokenId, name} of materials){
-    if(resources.hasOwnProperty(name) && !isNaN(parseFloat(resources[name]))){
-      totalBuy+= resources[name] * prices[name].highestBuy
-      totalSell+= resources[name] * prices[name].lowestSell
-    }
-  }
-
-  const totalBuyUsd = totalBuy * samaPrice
-  const totalSellUsd = totalSell * samaPrice
-  return {totalBuy: totalBuy.toFixed(3), totalBuyUsd: totalBuyUsd.toFixed(2), totalSell: totalSell.toFixed(3), totalSellUsd: totalSellUsd.toFixed(2)}
-}
-
-
-
-
-// Date
-
-/**
- * Make a date more human-readable
- * @param {Date} date: the date that needs to be prettified (3 April 2022 format)
- * @returns {String}
- */
-function formatDate(date){
-  //keep time zone in UTC so date shows carnage dates as reflected on moonsama website
-  const options = {year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC'}
-  return new Date(date).toLocaleDateString("en-GB", options)
-}
-
-/**
- * Format date for api request
- * @param {Date} date: the date that needs to be prettified (yyyy-mm-dd format)
- * @returns {String}
- */
-function formatDateApi(date){
-  return new Date(date).toISOString().split('T')[0]
-}
-
-/**
- * Capitalize and rename resources for human friendly display
- * @param {String}
- * @returns {String}
- */
-function prettifyResource(resource){
-  let res = resource
-
-  if(res === "fish_specimen"){
-    res = "fish"
-  }
-
-  return res.trim().toLowerCase().replace(/\w\S*/g, (w) => (w.replace(/^\w/, (c) => c.toUpperCase())))
-}
-
-
-/**
- * @returns {number[]}
- */
-function carnageDates() {
-  //2022-04-03 first date that carnage api has, game finishes at 6pm UTC
-  const carnageStartTime = 1649008800000
-  let carnageTime = carnageStartTime
-  const nowTime = new Date().getTime()
-  const dates = [carnageTime];
-  while(true){
-    carnageTime = carnageTime + 1000 * 60 * 60 * 24 * 7
-
-    if(carnageTime > nowTime){
-      break
-    }
-    dates.push(carnageTime)
-  }
-  return dates.reverse()
-}
-
-
-/**
- * Add thousands separators
- * @param {Number}
- * @returns {String}
- */
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-/**
- * Gets valid resources
- * @param {Object}
- * @returns {String[]}
- */
-function getValidResources(resources) {
-  const validResources = []
-    for(const [resource, value] of Object.entries(resources)){
-      if(!isNaN(parseFloat(value))){
-        validResources.push(resource)
-      }
-    }
-    // console.log(validResources)
-    return validResources.sort()
-}
-
-// Make the following functions accessible from AlpineJS
-
-// window.getAllMaterialsMovr = getAllMaterialsMovr
-window.getAllMaterialsMovr = getAllMaterialsMovr
-
-//load material quote on page load
-// window.getAllMaterialsMovrFailed = false 
+// load material quote on page load
 window.getAllMaterialsSamaFailed = false 
-
-// const getAllMaterialsMovrPromise = getAllMaterialsMovr()
 const getAllMaterialsSamaPromise = getAllMaterialsSama()
-
-// getAllMaterialsMovrPromise.catch(()=>{
-//   window.getAllMaterialsMovrFailed = true
-// })
 getAllMaterialsSamaPromise.catch(()=>{
   window.getAllMaterialsSamaFailed = true
 })
-
-// window.getAllMaterialsMovrPromise = getAllMaterialsMovrPromise
+window.getAllMaterialsSama = getAllMaterialsSama
 window.getAllMaterialsSamaPromise = getAllMaterialsSamaPromise
-
-// window.getTotal = getTotal
 window.getTotalSama = getTotalSama
 window.getUSDPriceOfSama = getUSDPriceOfSama
 window.formatDate = formatDate
@@ -363,6 +348,7 @@ window.carnageDates = carnageDates
 window.numberWithCommas = numberWithCommas
 window.getValidResources = getValidResources
 window.prettifyResource = prettifyResource
+
 // Load Alpine
 window.Alpine = Alpine
 Alpine.start()
--- src/js/queryprices.js
+++ src/js/queryprices.js
@@ -3,7 +3,8 @@ const axios = require('axios'); //
 var khaosSubgraphQueries = {
   all_pairs_query: "query MyQuery($resources_addresses: [String!]) {tokens {pairQuote {token0 {id symbol} \ntoken1 {id \nsymbol} \ntoken0Price \ntoken1Price}}}",
   rss_usdc_pair_query: "query MyQuery($resources_addresses: [String!]) {tokens {pairQuote(where: {token0_: {symbol_in: $resources_symbols}, token1_: {symbol: \"USDC\"}}) {token0 {\nid\nsymbol}token1 {id symbol}\ntoken0Price\ntoken1Price}}}",
-  rss_sama_pair_query: "query MyQuery($resources_addresses: [String!]) { tokens { pairQuote(where: {token0_in: $resources_addresses, token1: \"0x8c992cba48189a79204223d106fcb1d797a5f87a\"}) { token0 { id symbol } token1 { id symbol } token0Price token1Price } }}",
+  all_rss_sama_pair_query: "query MyQuery($resources_addresses: [String!]) { tokens { pairQuote(where: {token0_in: $resources_addresses, token1: \"0x8c992cba48189a79204223d106fcb1d797a5f87a\"}) { token0 { id symbol } token1 { id symbol } token0Price token1Price } }}",
+  rss_sama_pair_query: "query MyQuery($rss: String!) { tokens { pairQuote(where: {token0: $rss, token1: \"0x8c992cba48189a79204223d106fcb1d797a5f87a\"}) { token0 { id symbol } token1 { id symbol } token0Price token1Price } }}",
   usdc_sama_pair_query: "query MyQuery { tokens { pairQuote(where: {token0: \"0x765277eebeca2e31912c9946eae1021199b39c61\", token1: \"0x8c992cba48189a79204223d106fcb1d797a5f87a\"}) { token0 { id symbol } token1 { id symbol } token0Price token1Price } }}"
 };
 
@@ -24,18 +25,18 @@ async function getUSDPriceOfSama() {
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"'
   };
-  let queryString = khaosSubgraphQueries.usdc_sama_pair_query; 
+  let queryString = khaosSubgraphQueries.usdc_sama_pair_query;
 
   var sama_price = -1;
   const response = await performQuery(url, null, headers, queryString);
-  const data = response.data.data.tokens; 
-  for (let i=0; i < data.length; i++ ) {
+  const data = response.data.data.tokens;
+  for (let i = 0; i < data.length; i++) {
     if (data[i].pairQuote.length != 0) {
       sama_price = data[i].pairQuote[0].token0Price;
     }
   }
   console.log(sama_price);
-  return sama_price; 
+  return sama_price;
 }
 
 async function getUSDPriceOfRss() {
@@ -55,37 +56,83 @@ async function getUSDPriceOfRss() {
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"'
   };
-  let queryString = khaosSubgraphQueries.rss_sama_pair_query; 
+  let queryString = khaosSubgraphQueries.all_rss_sama_pair_query;
   let variables = { resources_addresses: ["0x549ab43056e3489335b47927ffcd4825ce484ba3", "0x122ebe2b679cf54bc8a6e89c1009714b354e2d10", "0x0528ae6c997ba4ffc4d6add898a2cc9f26d4a872", "0x829191fec0f25a27c5bdcdbbd0e37e6342b39945"] };
-  
+
   let sama_usd_price = await getUSDPriceOfSama();
   var rss_prices_usd = {};
   const response = await performQuery(url, variables, headers, queryString);
-  let data = response.data.data.tokens; 
-  for (i=0; i < data.length; i++ ) {
+  let data = response.data.data.tokens;
+  for (i = 0; i < data.length; i++) {
     if (data[i].pairQuote.length != 0) {
-      for (j=0; j < data[i].pairQuote.length; j++ ) {
-        let rss_name = data[i].pairQuote[j].token0.symbol; 
-        rss_prices_usd[rss_name] = data[i].pairQuote[j].token1Price * sama_usd_price; 
+      for (j = 0; j < data[i].pairQuote.length; j++) {
+        let rss_name = data[i].pairQuote[j].token0.symbol;
+        rss_prices_usd[rss_name] = data[i].pairQuote[j].token1Price * sama_usd_price;
       }
     }
   }
   console.log(rss_prices_usd)
-  return rss_prices_usd; 
+  return rss_prices_usd;
 }
-  
+
+async function getPriceOfARssInSama(address) {
+  console.log("Getting price of a rss in sama...");
+  let url = 'https://graph.khaos.exchange/subgraphs/name/exosama-dex';
+  let headers = {
+    'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,it;q=0.6',
+    'Connection': 'keep-alive',
+    'Origin': 'https://analytics.khaos.exchange',
+    'Referer': 'https://analytics.khaos.exchange/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'accept': '*/*',
+    'content-type': 'application/json',
+    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"'
+  };
+  let queryString = khaosSubgraphQueries.rss_sama_pair_query;
+  let variables = { rss: address };
+
+  let price_in_sama = 0; 
+  let rss_name = null; 
+
+  console.log("Query...")
+  console.log("variables: " + variables)
+  console.log("address: " + address)
+  const response = await Promise.resolve(performQuery(url, variables, headers, queryString).catch(err => console.error(err)));
+  let data = response.data.data.tokens;
+  console.log(data)
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].pairQuote.length != 0) {
+      console.log(" DATA FOUND..." +  data[i])
+      price_in_sama = data[i].pairQuote[0].token1Price;
+      rss_name = data[i].pairQuote[0].token0.symbol;
+    }
+  }
+  return price_in_sama;
+}
+
+
 async function performQuery(url, variables, headers, queryString) {
+  console.log("performing query...")
   const response = await axios.post(
     url,
     {
-        'variables': variables,
-        'query': queryString 
+      'variables': variables,
+      'query': queryString
     },
     {
-        headers
+      headers
     }
   );
-  return response; 
+  return response;
 }
+// let q = "0x122EBE2B679cF54Bc8a6e89c1009714b354e2d10".toLowerCase()
+// console.log(q)
+// getPriceOfARssInSama(q)
 
-export { getUSDPriceOfRss, getUSDPriceOfSama }; 
+export { getUSDPriceOfRss, getUSDPriceOfSama, getPriceOfARssInSama }; 
